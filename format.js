80269b42bbe9756a8e8e99086e22d6959d972835
macalan <chele@glassbell.com>
2018-05-07 10:53:51 -0400

    format.js complete

--- format.js
+++ format.js
@@ -4,4 +4,19 @@ the white space between them HINT: this regex might come in useful in the replac
 /\s\s+/g
 */
 
+function format (names) {
+  let newNames = names.map((val, idx, arr) => {
+    let re = /\s\s+/g;
+    let [a, b] = val.trim().split(re);
+    [b, a] = [a, b];
+    return `${a} ${b}`;
+  })
+//  console.log(newNames);
+}
 
+// let names = [
+//   "  Wilson   Janice  ",
+//   "   Redway    Michele  "
+// ]
+
+// format(names);
