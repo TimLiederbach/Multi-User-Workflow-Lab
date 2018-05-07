c8d1714f0ee4097a5418e1ffd3348485260e41fa
Student_105 <student_105@ool-ad020213.dyn.optonline.net>
2018-05-07 10:20:30 -0400

    bring in required files

--- shuffle.js
+++ shuffle.js
@@ -3,4 +3,9 @@ Import the files you need to run this file and then write the final function shu
 The function should write a file called studentGroups.txt
 and console log the final array of groups.
 */
+const format       = require('./format');
+const randomGroups = require('./randomGroups');
+const writeToFile  = require('./writeToFile');
+const students     = require('./students');
+
 
