1601c2b92761f20014d1662c4dd945fa70b50268
tim liederbach <timmy@ool-ad020772.dyn.optonline.net>
2018-05-07 10:31:51 -0400

    writeToFile Complete

--- writeToFile.js
+++ writeToFile.js
@@ -4,4 +4,14 @@ studentGroups.txt to the directory from which it's called. You will need to requ
 node fs object and use the fs.writeFile method. Google it.
 */
 
+//Requires file system
 const fs = require('fs');
+
+//Variable that can be changed to fit any string value
+const string = 'Our String Value'
+
+//Function that writes string to studentsGroups.txt
+fs.writeFile('studentGroups.txt', string, function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});
